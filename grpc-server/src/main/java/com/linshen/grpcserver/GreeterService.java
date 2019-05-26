9c660d2560a342ef16fc41e3c207a50d640e1cc0
LH <m15510800364@163.com>
2019-05-26 13:46:28 +0800

    gRPC

--- grpc-server/src/main/java/com/linshen/grpcserver/GreeterService.java
+++ grpc-server/src/main/java/com/linshen/grpcserver/GreeterService.java
@@ -7,15 +7,28 @@ import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.springboot.autoconfigure.grpc.server.GrpcService;
 
+import javax.imageio.stream.FileImageOutputStream;
+import java.io.File;
+
 @Slf4j
 @GrpcService(GreeterOuterClass.class)
 public class GreeterService extends GreeterGrpc.GreeterImplBase {
     @Override
     public void sayHello(GreeterOuterClass.HelloRequest request, StreamObserver<GreeterOuterClass.HelloReply> responseObserver) {
-        String message = "Hello " + request.getName();
+        String message = "Hello " + request.getImage();
+        byte[] bytes=request.getImage().toByteArray();
+        String path=request.getName();
+        try{
+                 FileImageOutputStream imageOutput = new FileImageOutputStream(new File(path));
+                 imageOutput.write(bytes, 0, bytes.length);
+                 imageOutput.close();
+                 System.out.println("Make Picture success,Please find image in " + path);
+                 } catch(Exception ex) {
+                   System.out.println("Exception: " + ex);
+                   ex.printStackTrace();
+                 }
         final GreeterOuterClass.HelloReply.Builder replyBuilder = GreeterOuterClass.HelloReply.newBuilder().setMessage(message);
         responseObserver.onNext(replyBuilder.build());
         responseObserver.onCompleted();
-        System.out.println("Returning "+message);
     }
 }
